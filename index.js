f3e64b730030604c1d9b44876dde8cea404b55e4
Damini Sharma <daminisharma@cs.uchicago.edu>
2019-09-11 19:05:41 +0530

    Working gender and race breakdown on mouseover

--- index.js
+++ index.js
@@ -37,12 +37,279 @@ var svg = d3.select("body").append("svg")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     //d3.json("processed_data/intake_year_status_gender.json")
 
-d3.json("processed_data/intake_year_status_gender.json")
+Promise.all([
+  d3.json("processed_data/intake_year_race_gender.json"),
+  d3.json("processed_data/intake_year_status_race.json"),
+  d3.json("processed_data/intake_year_race_gender.json"),
+]).then(function(allData) {
+  makeVis(allData);
+});
+
+function makeVis(allData) {
+  nested_data = nested(allData[0]);
+  //console.log(RaceGender);
+
+  // nestGender.forEach(function (d) {
+  //   d.values.forEach(function (d, m) {
+  //     //console.log(nestRace[m].values[m].value);
+  //     //console.log(d.value);
+  //   });
+    
+  // });
+  barChart(nested_data);
+};
+
+function nested(dataset) {
+      nested_total = d3.nest()
+            .key(function(d) {
+                return d.Year;
+            })
+            .key(function(d){
+                return d.initiation_result;
+            })
+            .rollup(function(leaves) {
+              return [{
+                key: 'Female',
+                value: leaves[0]['Female']
+              }, {
+                key: 'Male',
+                value: leaves[0]['Male']
+              },{
+                key: 'Black',
+                value: leaves[0]['Black']
+              }, {
+                key: 'White',
+                value: leaves[0]['White']
+              },
+              {
+                key: 'Latinx',
+                value: leaves[0]['Latinx']
+              },
+              {
+                key: 'Other',
+                value: leaves[0]['Other']
+              },{
+                key: 'Unknown',
+                value: leaves[0]['Unknown']
+              }];
+            })
+            .entries(dataset);
+
+            nested_total.forEach(function(d){
+            d.values.forEach(function(d) {
+              //console.log(d.value[0].value);
+              d.Total = d.value[0].value + d.value[1].value; 
+              d.Gender = [d.value[0],d.value[1]];
+              d.Race = [d.value[2],d.value[3],d.value[4],d.value[5],d.value[6]];
+            })  
+          });
+        console.log(nested_total);
+        return nested_total;
+};
+
+// make bar chart
+function barChart(dataset) {
+  dataset.forEach(function(d){
+    d.values.forEach(function(d) {
+      //console.log(d.value[0].value);
+      d.Total = d.value[0].value + d.value[1].value; 
+    })  
+  });
+
+  x_groups.domain(dataset.map(function(d) { return d.key; }));
+
+  var results = dataset[0].values.map(function(d, i) {
+      //console.log(d.key);
+      return d.key;
+    });
+  
+    x_categories.domain(results).rangeRound([0, x_groups.bandwidth()]);
+  
+  //var values = ['value 1', 'value 2', 'value 3']; 
+  //console.log(nested_total[0].values[0]);
+  var values = "Total";
+  //console.log(values);
+
+  x_values.domain(values).rangeRound([0, x_categories.bandwidth()]);
+  
+  y.domain([0, d3.max(dataset, function(d) {
+      //console.log(d.values);
+      return d3.max(d.values, function(d) {
+          //console.log(d.Total); 
+          return d.Total;                   
+      })
+    })]);
+  
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (height) + ")")
+    .call(groups_axis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 1)
+    .attr("dy", ".5em")
+    .style("text-anchor", "end")
+    .text("Value");
+
+  var groups_g = svg.selectAll(".group")
+    .data(dataset)
+    .enter().append("g")
+    .attr("class", function(d) {
+      return 'group group-' + d.key;
+    })
+    .attr("transform", function(d) {
+      return "translate(" + x_groups(d.key) + ",0)";
+    });
+
+  var categories_g = groups_g.selectAll(".category")
+    .data(function(d) {
+      //console.log(d.key);
+      return d.values;
+    })
+    .enter().append("g")
+    .attr("class", function(d) {
+      return 'category category-' + d.key;
+    })
+    .attr("transform", function(d) {
+      return "translate(" + x_categories(d.key) + ",0)";
+    })
+  
+
+  var rects = categories_g.selectAll('.rect')
+    .data(function(d) {
+      //console.log(m);
+      return [d];
+    })
+    .enter().append("rect")
+    .attr("class", "rect")
+    .attr("width", 15)
+    .attr("x", function(d) {
+      return 0;
+    })
+    .attr("y", function(d) {
+      return y(d.Total);
+    })
+    .attr("height", function(d) {
+      return height - y(d.Total);
+    })
+    .on("mouseover",mouseover)
+    .on("mouseout",mouseout);// mouseout is defined below.
+
+};
+
+function mouseover(d){  // utility function to be called on mouseover.
+  // filter for selected state.
+  console.log(d.Gender);
+  gender = d.Gender;
+  race = d.Race;
+  //console.log(d);
+  //console.log(nestRace[m].values[m].value);
+  //console.log(d.value);
+  //var result = nested_total.filter(function(s){ return s.key == d[0];})[0],
+  //nD = d3.keys(st.freq).map(function(s){ return {type:s, freq:st.freq[s]};});
+     
+  // call update functions of pie-chart and legend.    
+pC.update(gender,"on");
+pC.update(race,"on");
+}
+
+function mouseout(d){
+  pC.update(d,"off");
+
+}
+
+var pC = {};
+
+pC.update = function(data, mouse){
+
+  var w = 200;
+  var h = 200;
+
+  var outerRadius = w/2;
+  var innerRadius = w/3;
+  var arc = d3.arc()
+  .innerRadius(innerRadius)
+  .outerRadius(outerRadius);
+  
+  var pie = d3.pie().value(function(d) { return d.value ;} );
+
+  var pie_svg = d3.select('body')
+      .append("svg")
+      .attr("class","pie")
+      .attr('width',w)
+      .attr('height',h);
+  
+  //console.log(pie(nested_gender_test));
+  // Set up groups
+  var arcs = pie_svg.selectAll("g.arc")
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .attr("class","arc")
+    .attr("transform", "translate(" + +outerRadius + "," + +outerRadius + ")");
+
+  if (mouse === "on") {
+    
+
+    // Draw arcs paths
+    arcs.append("path")
+        .attr("fill",function(d,i) {
+          return color(i);
+        })
+        .attr("d",arc);
+
+
+  }
+
+  else if (mouse === "off") {
+    d3.selectAll(".pie")
+      .transition()
+      .duration(1)
+      .attr('opacity',0)
+      .remove();
+  }
+};
+
+
+
+
+/*
+d3.json("processed_data/intake_year_race_gender.json")
         .then(function(data) {
             
             dataset = data;
             
-            
+            nested_test = [];
+
+            nested_test_1 = d3.nest()
+            .key(function(d) {
+                return d.Year;
+            })
+            .key(function(d){
+                return d.initiation_result;
+            })
+            .entries(dataset);
+
+            //console.log(nested_test_1);
+            nested_test_1.forEach(function (a) {
+              //console.log(a);
+              a.values.forEach(function (a) {
+                //console.log(a);
+                if (!this[a.initiation_result] ) {
+                  this[a.initiation_result] = {key: a.initiation_result, value: []}; 
+                  nested_test.push(this[a.initiation_result]);
+                }
+
+                
+              })
+            })
+            console.log('nested test');
+            console.log(nested_test);
+
             //console.log(dataset);
             nested_total = d3.nest()
             .key(function(d) {
@@ -69,7 +336,7 @@ d3.json("processed_data/intake_year_status_gender.json")
             })  
           });
 
-            console.log(nested_total);
+            //console.log(nested_total);
 
             // var keys = d3.keys(data[0]).slice(1);
             // console.log(d3.max(data, function(d) {return d.Year}));
@@ -159,7 +426,7 @@ d3.json("processed_data/intake_year_status_gender.json")
             //   .text(function(d) {
             //     return d;
             //   })
-            /*  
+            /*
             var values_g = categories_g.selectAll(".value")
               .data(function(d) {
                 //console.log(d.Total);
@@ -193,6 +460,7 @@ d3.json("processed_data/intake_year_status_gender.json")
             //   })
           
             //console.log(values_g);
+            /*
             var rects = categories_g.selectAll('.rect')
               .data(function(d) {
                 //console.log([d]);
@@ -344,7 +612,7 @@ d3.json("processed_data/intake_year_status_gender.json")
 
 
 
-            
+            /*
             })
 
             .catch(function(error){
@@ -360,7 +628,7 @@ d3.json("processed_data/intake_year_status_gender.json")
 
 next steps: figure out if nested sructure allows for pie chart to be created on the side
 http://bl.ocks.org/cflavs/695d3215ccbce135d3bd
-
+http://bl.ocks.org/cflavs/695d3215ccbce135d3bd
 
 if bar chart and pie chart working interactively, clean up code
 add dispositions (and sentencing?) data plus nested structures
@@ -369,6 +637,11 @@ add functions to choose different parts of the data to be turned into bar chart
 change categories for intake - other too small
 
 
+next steps:
+- Add headings, labels, titles, roughly
+- Clean up code!!!!!
+- Add more dimensions: add gender, and append dispositions
+- Clean up intake categories 
 
 */
 
@@ -741,7 +1014,3 @@ console.log(svg.attr('height'));
     var z = d3.scaleOrdinal()
         .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 */
-
-
-
-
